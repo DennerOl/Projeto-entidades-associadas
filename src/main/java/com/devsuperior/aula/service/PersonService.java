292adcdfbe95349c2015c833e8aa77d31b2c4271
DennerOl <dener155@hotmail.com>
2023-06-14 21:26:28 -0300

    Entidades associadas PersonDTO

--- src/main/java/com/devsuperior/aula/service/PersonService.java
+++ src/main/java/com/devsuperior/aula/service/PersonService.java
@@ -3,6 +3,7 @@ package com.devsuperior.aula.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.aula.dto.PersonDTO;
 import com.devsuperior.aula.dto.PersonDepartmentDTO;
 import com.devsuperior.aula.entities.Department;
 import com.devsuperior.aula.entities.Person;
@@ -20,7 +21,8 @@ public class PersonService {
 
 /* primeiro, transformo um DTO em uma entidade para inserir no banco
  * segundo, como as entidades são associadas eu busco no banco 
- * o Id referente do departamento	
+ * o Id referente do departamento
+ * aqui no PersonDepartmentDTO eu tenho um DepartmentDTO alinhado	
  */
 	public PersonDepartmentDTO insert(PersonDepartmentDTO dto) {		
 		Person entity = new Person();
@@ -36,5 +38,24 @@ public class PersonService {
 		entity = repository.save(entity);
 		return new PersonDepartmentDTO(entity);
 		
+	}
+	
+/* aqui não tenho mais o DepartmentDTO alinhado 
+ * 	so passo o id 
+ */
+	public PersonDTO insert(PersonDTO dto) {		
+		Person entity = new Person();
+		entity.setName(dto.getName());
+		entity.setSalary(dto.getSalary());
+
+		Department dept = departamentRepository.getReferenceById(dto.getDepartmentId());
+		
+		// Department dept = new Department();
+		//dept.setId(dto.getDepartmentId());
+		
+		entity.setDepartment(dept);
+		
+		entity = repository.save(entity);
+		return new PersonDTO(entity);
 	}
 }
